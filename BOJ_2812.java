cf4487b13ca705e381403794c50b7aef0086cfc0
sangjun-Park1208 <77184523+sangjun-Park1208@users.noreply.github.com>
2022-07-12 18:37:55 +0900

    BOJ_2812

--- BOJ_2812.java
+++ BOJ_2812.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class BOJ_2812 {
@@ -19,30 +20,21 @@ public class BOJ_2812 {
         int[] I = new int[N];
         for(int i=0; i<N; i++) { I[i] = input.charAt(i) - 48;}
 
-        Stack<Integer> S = new Stack<>();
+        Deque<Integer> S = new ArrayDeque<>();
         int deleteCnt = 0;
         S.add(I[0]);
         for(int i=1; i<N; i++){
-            if(deleteCnt == K){
-                S.add(I[i]);
+            while(S.size() > 0 && deleteCnt < K && S.peekLast() < I[i]){
+                S.pollLast();
+                deleteCnt++;
             }
-            else{
-                while(S.size() > 0 && S.peek() < I[i]){
-                    S.pop();
-                    deleteCnt++;
-                    if(deleteCnt == K)
-                        break;
-                }
-                S.add(I[i]);
-            }
+            S.add(I[i]);
         }
 
-
         int[] ans = new int[N-K];
-        int j=N-K-1;
-        while(S.size() > 0){
-            ans[j] = S.pop();
-            j--;
+        int j=0;
+        while(S.size()>0 && j<N-K){
+            ans[j++] = S.pollFirst();
         }
         for(int i=0; i<N-K; i++){ bw.write(String.valueOf(ans[i])); }
         bw.flush();
