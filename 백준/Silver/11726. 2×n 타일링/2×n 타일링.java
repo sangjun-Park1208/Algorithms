3088eeca0f713f38b6ecd47fe378a800848da1a0
Sangjun Park <sangjun1997@naver.com>
2022-08-01 00:24:21 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] DP = new int[1001];
+        DP[0] = DP[1] = 1;
+        for(int i=2; i<=n; i++){
+            DP[i] = (DP[i-1] + DP[i-2]) % 10007;
+        }
+        bw.write(String.valueOf(DP[n]));
+        bw.flush();
+    }
+}
