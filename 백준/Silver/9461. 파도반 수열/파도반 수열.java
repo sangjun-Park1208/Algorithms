7cb135aafa6304143bf696cbe83c4ddb0dadf66d
Sangjun Park <sangjun1997@naver.com>
2022-08-01 00:16:01 +0900

    [Silver III] Title: 파도반 수열, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int D = 0;
+        
+
+        for(int i=1; i<=N; i++){
+            D = Integer.parseInt(br.readLine());
+            long [] P = new long[101];
+            
+            P[1] = 1; P[2] = 1; P[3] = 1; P[4] = 2; P[5] = 2;
+
+            for(int j=6; j<=D; j++){ P[j] = P[j-1] + P[j-5]; }
+
+            bw.write(String.valueOf(P[D])  + "\n");
+        }        
+        bw.flush();
+    }
+}
