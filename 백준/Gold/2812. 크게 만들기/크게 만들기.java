d046e0fb3712ead59d3dc37bc7594ca6e41e2ebe
Sangjun Park <sangjun1997@naver.com>
2022-08-01 00:13:13 +0900

    [Gold III] Title: 크게 만들기, Time: 416 ms, Memory: 35380 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        String input = br.readLine();
+        int[] I = new int[N];
+        for(int i=0; i<N; i++) { I[i] = input.charAt(i) - 48;}
+
+        Deque<Integer> S = new ArrayDeque<>();
+        int deleteCnt = 0;
+        S.add(I[0]);
+        for(int i=1; i<N; i++){
+            while(S.size() > 0 && deleteCnt < K && S.peekLast() < I[i]){
+                S.pollLast();
+                deleteCnt++;
+            }
+            S.add(I[i]);
+        }
+
+        int[] ans = new int[N-K];
+        int j=0;
+        while(S.size()>0 && j<N-K){
+            ans[j++] = S.pollFirst();
+        }
+        for(int i=0; i<N-K; i++){ bw.write(String.valueOf(ans[i])); }
+        bw.flush();
+    }
+}
