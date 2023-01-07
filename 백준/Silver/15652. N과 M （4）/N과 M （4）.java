1fa8a92aab97380dc58df90e30dbad3e4f70a274
Sangjun Park <sangjun1997@naver.com>
2023-01-08 00:38:31 +0900

    [Silver III] Title: N과 M (4), Time: 216 ms, Memory: 18980 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+    static int N, M;
+    static int[] Ans;
+    static boolean[][] Visit;
+    public static void main(String[] args) throws IOException{
+        getInput();
+        Solve(0, 1);
+        bw.flush();
+    }
+    
+    static void getInput() throws IOException{
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Ans = new int[M];
+    }
+
+    static void Solve(int depth, int pos) throws IOException{
+        if(depth == M){
+            for(int i=0; i<M; i++) 
+                bw.write(Ans[i] + " ");
+            bw.write("\n");
+            return;
+        }
+        for(int i=pos; i<=N; i++){
+            Ans[depth] = i;
+            Solve(depth+1, i);
+        }
+    }
+}
