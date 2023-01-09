e7075000a26d88f5695e11d9653f3ab764b9af86
Sangjun Park <sangjun1997@naver.com>
2023-01-09 20:19:19 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14584 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N, ans;
+    static int[] input, D;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        getInput();
+        Solve();
+    }
+
+    static void Solve() {
+        for(int i=0; i<N; i++)
+            for(int j=0; j<i; j++)
+                if(input[j] > input[i])
+                    D[i] = Math.max(D[j]+1, D[i]);
+        
+        ans = D[0];
+        for(int i=1; i<N; i++) ans = Math.max(ans, D[i]);
+        System.out.println(ans);   
+    }
+
+    static void getInput() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        input = new int[N];
+        D = new int[N];
+        for(int i=0; i<N; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+            D[i] = 1;
+        }
+    }
+}
