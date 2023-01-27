88f078e136df5e45ce0f28db3ea52a89e7494d85
Sangjun Park <sangjun1997@naver.com>
2023-01-27 18:15:34 +0900

    [Gold V] Title: 택배 배송, Time: 456 ms, Memory: 38404 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N, M, a, b, c, next, tmp;
+    static int[] min;
+    static ArrayList<Node>[] graph;
+    static PriorityQueue<Node> pq; 
+    static int INF = 987654321;
+    static boolean[] selected;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        getInput();
+        Solve();
+    }
+
+    static class Node {
+        int index;
+        int dist;
+        public Node(int index, int dist) {
+            this.index = index;
+            this.dist = dist;
+        }
+    }
+
+    static void Solve() {
+        pq = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node n1, Node n2){
+                return n1.dist - n2.dist;    
+            }
+        });
+        pq.add(new Node(1, 0));
+        
+        while(!pq.isEmpty()){
+            Node current = pq.poll();
+            if(selected[current.index]) continue;
+            selected[current.index] = true;
+            
+            for(int i=0; i<graph[current.index].size(); i++){
+                Node next = graph[current.index].get(i);
+                if(min[current.index] + next.dist < min[next.index]){
+                    /* 최소 비용 갱신 */
+                    min[next.index] = min[current.index] + next.dist;
+                    pq.add(new Node(next.index, min[next.index]));
+                }
+            }
+        }
+        System.out.println(min[N]);
+    }
+
+
+    static void getInput() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) graph[i] = new ArrayList<Node>();
+        
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
+        }
+        min = new int[N+1];
+        selected = new boolean[N+1];
+        Arrays.fill(min, INF);
+        min[1] = 0;
+    }
+}
