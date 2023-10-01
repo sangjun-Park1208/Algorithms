452939674c47e7c20020296372835bb06a439092
Sangjun Park <sangjun1997@naver.com>
2023-10-02 02:39:32 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        answer = 1;
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            answer *= entry.getValue() + 1;
+        }
+        return answer - 1;
+    }
+}
