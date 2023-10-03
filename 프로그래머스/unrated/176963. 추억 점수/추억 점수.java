b3643fbbf34c02b5910857faff5a4d558b734908
Sangjun Park <sangjun1997@naver.com>
2023-10-04 01:12:36 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<photo.length; i++) {
+            for(int j=0; j<photo[i].length; j++) {
+                map.put(photo[i][j], 0);
+            }
+        }
+        
+        for(int i=0; i<name.length; i++) {
+            map.put(name[i], yearning[i]);    
+        }
+        
+        
+        int sum;
+        for(int i=0; i<photo.length; i++) {
+            sum = 0;
+            for(int j=0; j<photo[i].length; j++) {
+                sum += map.get(photo[i][j]);
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
