581dc59a7b88056b21717dc5c908d13acbaaf61a
Sangjun Park <sangjun1997@naver.com>
2023-10-07 23:35:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 140 ms, Memory: 16896 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,80 +1,68 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
-    static int N, min, max, tmp;
-    static int[] A, op, count, input;
-    static boolean[] visit;
-    
-    public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        getInput();
-        Solve(0);
-        bw.write(max + "\n" + min);
+    static long max, min;
+    static int N;
+    static int[] numbers;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        numbers = new int[N];
+        for(int i=0; i<N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] operations = new int[4];
+        for(int i=0; i<4; i++) {
+            operations[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        max = -1000000001L;
+        min = 10000000000L;
+
+        backtracking(operations, numbers[0], 0);
+        bw.write(String.valueOf(max) + "\n");
+        bw.write(String.valueOf(min));
         bw.flush();
+        
+
     }
 
-    static void Solve(int depth) {
+    static void backtracking(int[] operations, long num, int depth) {
+        // 종료조건
         if(depth == N-1) {
-            tmp = A[0];
-            for(int i=1; i<N; i++){
-                switch (input[i-1]) {
-                    case 0:
-                        tmp += A[i];
-                        break;
-                    case 1:
-                        tmp -= A[i];
-                        break;
-                    case 2:
-                        tmp *= A[i];
-                        break;
-                    case 3:
-                        tmp /= A[i];
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if(tmp > max) max = tmp;
-            if(tmp < min) min = tmp;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
             return;
         }
 
-        for(int i=0; i<N-1; i++){
-            if(visit[i]) continue;
-            visit[i] = true; // 방문체크
-            input[depth] = count[i];
-            Solve(depth+1);
-            visit[i] = false;
+        for(int i=0; i<4; i++) {
+            if(operations[i] > 0) {
+                operations[i]--;
+                switch(i) {
+                    case 0:
+                        backtracking(operations, num + numbers[depth+1], depth+1);
+                        break;
+                    case 1:
+                        backtracking(operations, num - numbers[depth+1], depth+1);
+                        break;
+                    case 2:
+                        backtracking(operations, num * numbers[depth+1], depth+1);
+                        break;
+                    case 3:
+                        backtracking(operations, num / numbers[depth+1], depth+1);
+                        break;
+                }
+                operations[i]++;
+            }
         }
-    }
-
-    static void getInput() throws IOException {
-        N = Integer.parseUnsignedInt(br.readLine());
-        A = new int[N];
-        op = new int[4];
-        max = -987654321;
-        min = 987654321;
-
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) A[i] = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++) op[i] = Integer.parseInt(st.nextToken());
-
-        count = new int[N-1];
-        input = new int[N-1];
-        visit = new boolean[N-1];
-        int k = 0;
-        for(int i=0; i<4; i++)
-            for(int j=0; j<op[i]; j++, k++) count[k] = i;
     }
 }
