e80154a808700e9b00f5e4571b614a7f390d0f5b
Sangjun Park <sangjun1997@naver.com>
2023-11-03 23:55:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long A, B, C;
+
+int main() {
+    ios_base::sync_with_stdio; cin.tie(NULL);
+    cin >> A >> B >> C;
+    cout << A + B + C;
+    return 0;
+}
