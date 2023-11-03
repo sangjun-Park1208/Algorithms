4c089b9eeb12ed5213927c34fdac2bdae6328b7d
Sangjun Park <sangjun1997@naver.com>
2023-11-03 23:57:54 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio; cin.tie(NULL);
+
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|";
+    
+    return 0;
+}
