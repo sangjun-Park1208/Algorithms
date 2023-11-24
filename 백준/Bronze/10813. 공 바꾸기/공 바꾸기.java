fb0fd57ec327a90b7a4cff4dff2fdfea0e191739
Sangjun Park <sangjun1997@naver.com>
2023-11-24 18:23:58 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] baskets = new int[N+1];
+        for(int i=1; i<=N; i++) {
+            baskets[i] = i;
+        }
+
+        int a, b;
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            swap(baskets, a, b);
+        }
+
+        for(int i=1; i<=N; i++) {
+            bw.write(baskets[i] + " ");
+        }
+        bw.flush();
+        return;
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        return;
+    }
+    
+}
